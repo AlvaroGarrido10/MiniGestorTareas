840f6fcdf39c57272b6c48b8ce7a9ae0acf9ebe5
Alvaro <Alvaro@DESKTOP-00MKEAC>
2026-02-23 13:42:46 +0100

    Commit dia 1

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniGestorTareas.Models;
+
+namespace MiniGestorTareas.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Tarea> Tareas { get; set; }
+}
---
+++ Models/Tarea.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniGestorTareas.Models;
+
+public class Tarea
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(80, MinimumLength = 3)]
+    public string Title { get; set; } = string.Empty;
+
+    [StringLength(200)]
+    public string? Description { get; set; }
+
+    [Required]
+    public Priority Priority { get; set; }
+
+    public Status Status { get; set; } = Status.Pending;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [DataType(DataType.Date)]
+    public DateTime? DueDate { get; set; }
+}
+
+public enum Priority
+{
+    Low,
+    Medium,
+    High
+}
+
+public enum Status
+{
+    Pending,
+    InProgress,
+    Done
+}
